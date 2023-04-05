0ec81d268b35092d2297033b616ca0cf94ad2342
Andrii Votiakov <Votiakov.andrey@gmail.com>
2023-04-05 15:10:17 +0200

    Update main.tf

--- modules/kubernetes-addons/csi-secrets-store-provider-aws/main.tf
+++ modules/kubernetes-addons/csi-secrets-store-provider-aws/main.tf
@@ -1,9 +1,10 @@
 locals {
-  name      = try(var.helm_config.name, "csi-secrets-store-provider-aws")
-  namespace = try(var.helm_config.namespace, "kube-system")
+  name      = try(var.helm_config.name, "secrets-store-csi-driver-provider-aws")
+  namespace = try(var.helm_config.namespace, local.name)
 }
 
 resource "kubernetes_namespace_v1" "csi_secrets_store_provider_aws" {
+  count = try(var.helm_config["create_namespace"], true) && local.namespace != "kube-system" ? 1 : 0
   metadata {
     name = local.namespace
   }
@@ -12,15 +13,15 @@ resource "kubernetes_namespace_v1" "csi_secrets_store_provider_aws" {
 module "helm_addon" {
   source = "../helm-addon"
 
-  # https://github.com/aws/eks-charts/blob/master/stable/csi-secrets-store-provider-aws/Chart.yaml
+  # https://github.com/aws/secrets-store-csi-driver-provider-aws/blob/main/charts/secrets-store-csi-driver-provider-aws/Chart.yaml
   helm_config = merge(
     {
       name        = local.name
-      chart       = local.name
-      repository  = "https://aws.github.io/eks-charts"
-      version     = "0.0.3"
-      namespace   = kubernetes_namespace_v1.csi_secrets_store_provider_aws.metadata[0].name
-      description = "A Helm chart to install the Secrets Store CSI Driver and the AWS Key Management Service Provider inside a Kubernetes cluster."
+      repository  = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
+      chart       = "secrets-store-csi-driver-provider-aws"
+      version     = "0.2.0"
+      namespace   = local.namespace
+      description = "A Helm chart for the AWS Secrets Manager and Config Provider for Secret Store CSI Driver"
     },
     var.helm_config
   )
